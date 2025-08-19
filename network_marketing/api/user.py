350aad572a5258382567fc933823fcddca5d578a
BinyamKefela <binyamkefela196@gmail.com>
2025-08-19 23:16:26 +0300

    changed signup

--- network_marketing/api/user.py
+++ network_marketing/api/user.py
@@ -418,7 +418,7 @@ def update_user(request,id):
 def sign_up(request):
         if User.objects.filter(email=request.data.get("email")).count()>0:
             return Response({"error":"This email already exists in the system"},status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(phone=request.data.get("phone")).count()>0:
+        if User.objects.filter(phone_number=request.data.get("phone_number")).count()>0:
             return Response({"error":"This phone number already exists in the system"},status=status.HTTP_400_BAD_REQUEST)
         
         if request.data.get('referal_code'):
